e37c10a6d95fb98d9d5d54c7f31b6d4c4981222e
DanikMonday <daacostaf@unal.edu.co>
2023-01-23 09:50:52 -0500

    styles

--- to-do-list/src/components/Todo.js
+++ to-do-list/src/components/Todo.js
@@ -3,6 +3,8 @@ import Form from './Form.js'
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 import axios from 'axios';
+import { BsFlower3} from "react-icons/bs";
+import { BsFillTrashFill} from "react-icons/bs";
 
 
 
@@ -27,14 +29,15 @@ const Todo = ({todos,completeTodo, removeTodo, updateTodo}) => {
 
   return todos.map((todo, index) => (
       <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
+        <div className='icons-i'><BsFlower3/></div>
           <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-              {todo.text}
+              {todo.text}     
           </div>
           <div className='icons'>
-              <RiCloseCircleLine onClick={() => removeTodo(todo.id)}
-              className='delete-icon'/>
               <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text})}
               className='edit-icon' />
+              <BsFillTrashFill onClick={() => removeTodo(todo.id)}
+              className='delete-icon'/>
 
           </div>
       </div>
